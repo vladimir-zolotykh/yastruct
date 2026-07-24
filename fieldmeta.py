cd4c081c55c19a26f06c7ec0bae9b4ae4c0c0148
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-07-24 19:42:45 +0300

    Initial import

---
+++ fieldmeta.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+from typing import Self
+from pathlib import Path
+import struct
+from header import get_bbox, POLYGONS
+
+
+class Field:
+    def __init__(self, offset):
+        self.offset = offset
+
+    def fetch(self, instance):
+        raise NotImplementedError
+
+    def drop(self, instance, value):
+        raise NotImplementedError
+
+
+class FieldStr(Field):
+    def __init__(self, offset, fmt):
+        super().__init__(offset)
+        self.fmt = fmt
+
+    def fetch(self, instance, owner=None):
+        rng = slice(self.offset, self.offset + struct.calcsize(self.fmt))
+        t = struct.unpack_from(self.fmt, instance.view[rng])
+        return t[0] if len(t) == 1 else t
+
+    def drop(self, instance, value):
+        rng = slice(self.offset, self.offset + struct.calcsize(self.fmt))
+        instance.view[rng] = struct.pack(self.fmt, value)
+
+
+class FieldType(Field):
+    def __init__(self, offset, typ):
+        super().__init__(offset)
+        self.typ = typ
+
+    def fetch(self, instance):
+        rng = slice(self.offset, self.offset + self.typ._size)
+        return self.typ(instance.view[rng])
+
+    def drop(self, instance, value):
+        rng = slice(self.offset, self.offset + type(value)._size)
+        instance.view[rng] = value._view
+
+
+class FieldMeta(type):
+    def __new__(mcls, name, bases, ns):
+        off = 0
+        fields = []
+        for key, val in ns.items():
+            if key[:2] == "__" and key[-2:] == "__":
+                continue
+            if isinstance(val, str):
+                ns[key] = FieldStr(val, off)
+                off += struct.calcsize(val)
+            elif isinstance(val, FieldMeta):
+                ns[key] = FieldType(val, off)
+                off += val._size
+            else:
+                pass
+            fields.append(key)
+            off += struct.calcsize(val)
+        ns["_size"] = off
+        ns["_fields"] = fields
+        return super().__new__(mcls, name, bases, ns)
+
+
+class View:
+    def __init__(self, bytesdata: bytes | memoryview):
+        self.view = memoryview(bytesdata)
+
+    def __repr__(self):
+        args = ", ".join(f"{getattr(self, name)!r}" for name in self._fields)
+        return f"{type(self).__name__}({args})"
+
+    def write(self, path: str):
+        with open(path, "wb") as f:
+            f.write(self.view)
+
+    @classmethod
+    def from_file(cls, path: str) -> Self:
+        with open(path, "rb") as f:
+            return cls(f.read(cls.VIEW_SIZE))
+
+
+class Point(View):
+    x = "<d"
+    y = "<d"
+
+
+class Bbox(View):
+    xy1 = Point
+    xy2 = Point
+
+
+class Header(View):
+    magic = "<i"
+    bbox = Bbox
+    num_polygons = "<i"
+
+    @classmethod
+    def expected(cls) -> Self:
+        h = cls(bytearray(cls._size))
+        h.magic = 0x1234
+        h.bbox = get_bbox(POLYGONS)
+        h.num_polygons = len(POLYGONS)
+        return h
+
+
+SCHEMA_DAT = ".schema.dat"
+
+if __name__ == "__main__":
+    if Path(SCHEMA_DAT).exists():
+        h = Header.from_file(SCHEMA_DAT)
+        print(h)
+    else:
+        h = Header.expected()
+        h.write(SCHEMA_DAT)
+        print(f"{SCHEMA_DAT} has written")
